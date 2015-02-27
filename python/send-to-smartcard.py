059b30b4cbbd481ff9bf805ce80c28326de45ca6
linjunqi <linjunqi@xiaomi.com>
2015-02-27 23:48:29 +0800

    add smartcard commander
    Signed-off-by: linjunqi <linjunqi@xiaomi.com>

---
+++ python/send-to-smartcard.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+from smartcard.System import readers
+from smartcard.util import toHexString
+from smartcard.CardType import AnyCardType
+from smartcard.CardRequest import CardRequest
+
+import sys
+import binascii
+
+"""
+send apdu command via smartcard reader
+"""
+def send_smartcard_cmd(cmdstr_list):
+    cardtype=AnyCardType()
+    cardrequest=CardRequest(timeout=100000,cardType=cardtype)
+    cardservice=cardrequest.waitforcard()
+
+    conn=cardservice.connection
+    conn.connect()
+
+    count=1
+    result=list()
+    for cmdstr in cmdstr_list:
+        cmd=list((ord(x) for x in binascii.unhexlify(cmdstr)))
+        print ">>>%d %s" % (count,toHexString(cmd).replace(' ',''))
+        data, sw1, sw2=conn.transmit(cmd)
+
+        print "<<<%d data: %s sw: %02x%02x" % (count,toHexString(data).replace(' ',''),sw1,sw2)
+        count+=1
+
+    return result.append((data, sw1, sw2))
+
+
+"""
+usage example 1: <PATH-OF-ME>/send-to-smartcard.py 00A4040000
+usage example 2: <PATH-OF-ME>/send-to-smartcard.py 00A4040000 80CA9F7F00
+"""
+if __name__=='__main__':
+    if(len(sys.argv)<2):
+        print "usage: %s: <APDU> [,<APDU> [,<APDU>] ...]" % sys.argv[0]
+    else:
+        send_smartcard_cmd(sys.argv[1:])
+
