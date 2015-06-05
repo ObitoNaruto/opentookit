15a60c75eaa08b0886e99eed194cc8eaa40afded
linjunqi <linjunqi@xiaomi.com>
2015-06-05 10:51:13 +0800

    add hex to base64
    N/A
    
    Signed-off-by: linjunqi <linjunqi@xiaomi.com>

---
+++ python/hex-to-base64.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+__author__ = 'linjunqi'
+
+import sys
+import base64
+import binascii
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print "usage: %s <hex str>" % sys.argv[0]
+        sys.exit(1)
+    else:
+        hex_str = sys.argv[1]
+        base64_str = base64.b64encode(binascii.unhexlify(hex_str)).upper()
+        print "\n========= Result Begin ========="
+        print base64_str
+        print "========= Result END =========\n"
